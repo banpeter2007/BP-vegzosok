65d524c0bba6c658cf5d2d9ca9efd1c7b3aa996f
banpeter2007 <ban.peter@diak.szbi-pg.hu>
2025-11-24 14:38:41 +0100

    szar_lett

---
+++ BP-vegzosok/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BP_vegzosok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            // TXT betöltése gomb eseménykezelő
+            string filePath = "vegzosok.txt";
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Nem található a TXT fájl!");
+                return;
+            }
+            var sorok = File.ReadAllLines(filePath, Encoding.Default);
+            var fejlec = sorok[0].Split(';');
+            DataTable tabla = new DataTable();
+            foreach (var oszlop in fejlec)
+                tabla.Columns.Add(oszlop);
+            foreach (var sor in sorok.Skip(1))
+                tabla.Rows.Add(sor.Split(';'));
+            dataGridView1.DataSource = tabla;
+        }
+    }
+}
